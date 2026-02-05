a9155977af09f7c8bb0c2a981b84bd08cf684fe0
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-02-05 17:36:08 +0000

    add: bypass identity for now

---
+++ evaluation/brain/identity.py
@@ -0,0 +1,9 @@
+from evaluation.brain.modes import BrainMode
+from evaluation.brain.models.harmonicus import Harmonicus
+
+class IdentityHarmonicus(Harmonicus):
+        def __init__(self):
+            super().__init__(
+                model=None,
+                mode=BrainMode.BYPASS,
+            )
